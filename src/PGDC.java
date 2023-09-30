57b7f3063a2234136d66df6112bc19fb25bf8e72
Mihir Gorasiya <mihirpatel9408@gmail.com>
2023-09-30 11:04:28 -0400

    Exercise 8 task 2 completed - unused variable removed

--- src/PGDC.java
+++ src/PGDC.java
@@ -1,14 +1,9 @@
 public class PGDC {
-    int number1;
-    int number2;
-
-
     public int pgdc(int a, int b){
         if (a > b){
             a = a - b;
             return pgdc(b, a);
         }
-        System.out.println("");
         return a;
     }
 
